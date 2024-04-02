9a36942935f46654e22cf69217d0a195192d8366
Shaun <skichenbrand@gmail.com>
2024-04-02 15:51:02 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Opus UI Lessons
+
+A collection of lessons referenced on the Opus UI Website
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+//Opus
+import { loadApp } from '@intenda/opus-ui';
+
+//Component Libraries
+import '@intenda/opus-ui-components';
+
+//Styles
+import './main.css';
+
+(async() => {
+	const res = await fetch('/app.json')
+	const mdaPackage = await res.json();
+
+	loadApp({
+		mdaPackage,
+		config: {
+			env: 'development'
+		}
+	});
+})();
